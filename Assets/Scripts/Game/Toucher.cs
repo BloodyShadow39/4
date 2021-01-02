161e097c49175ea9ec4e39a235591510405fc981
TechesDePainture <kirillzelen00@gmail.com>
2021-01-03 01:00:15 +0200

    Создан класс игрового взаимодействия

---
+++ Assets/Scripts/Game/Toucher.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Events;
+
+namespace Game {
+    public class Toucher : MonoBehaviour {
+
+        [SerializeField]
+        private EventDispatcher _selected;
+/*
+        private void OnEnable() {
+            _select.OnEventHappened+=SetOnWay;
+        }
+
+        private void OnDisable() {
+            _select.OnEventHappened -= SetOnWay;
+        }
+*/
+        private void OnMouseDown() {
+            Debug.Log($"{transform.position.x},{transform.position.z}");
+            //InputController.Instance.SetTouch((int)transform.position.x, (int)transform.position.z);
+            _selected.Dispatch();
+        }
+
+        private void SetOnWay() {
+            bool tmp = false;
+            for (int i = 0; i < InputController.Instance.way.Count; i++) {
+                if ((InputController.Instance.way[i].x == (int)transform.position.x) && (InputController.Instance.way[i].y == (int)transform.position.z)) {
+                    tmp = true;
+                    break;
+                }
+            }
+            if (tmp) 
+                if(transform.position.y<-0.01f)
+                    transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
+            else
+                if (transform.position.y > -0.49f)
+                    transform.position = new Vector3(transform.position.x, -0.5f, transform.position.z);
+        }
+
+    }
+}
