a16a01c719d98eef1ae476bade7affe482b7f7c2
TechesDePainture <kirillzelen00@gmail.com>
2021-01-02 21:06:29 +0200

    Scriptable Map Создана
    Данный скрип хранит информацию о создании карты

---
+++ Assets/Scripts/Scriptable obj/Scriptable Map.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Values {
+
+    [CreateAssetMenu(fileName = "ScriptableString", menuName = "ScriptableString")]
+    public class ScriptableMap : ScriptableObject {
+        [SerializeField]//For testing
+        private int width;//Ўирина текущей карты
+        [SerializeField]//For testing
+        private int height;
+
+        public struct cell {
+            public int x;
+            public int y;
+            public GameObject item;
+            public bool availability;
+            public int type;
+
+            public cell(int dx,int dy, GameObject ditem, bool davailability, int dtype) {
+                x = dx;
+                y = dy;
+                item = ditem;
+                availability = davailability;
+                type = dtype;
+            }
+
+            public cell(cell dcell) {
+                x = dcell.x;
+                y = dcell.y;
+                item = dcell.item;
+                availability = dcell.availability;
+                type = dcell.type;
+            }
+        }
+
+        public List<cell> map;
+
+    }
+}
