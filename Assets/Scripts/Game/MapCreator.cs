fa78efa7dcccd6f5404123d8050a0da8ada74ffd
TechesDePainture <kirillzelen00@gmail.com>
2021-01-07 23:58:45 +0200

    Добавлена обработка на повтор обьектов

--- Assets/Scripts/Game/MapCreator.cs
+++ Assets/Scripts/Game/MapCreator.cs
@@ -32,16 +32,26 @@ namespace Game{
             //FormirateMap();
         }
         public void GenerateMap(){
-            for(int i=0;i<_loadMap.map.Count;i++){
+            bool isCreated;
+            for (int i=0;i<_loadMap.map.Count;i++){
                 for(int j=0;j<_loadMap.map[i].Count;j++){
-                    if(_loadMap.map[i][j]>=0){
-                        Toucher tmp=Instantiate(_touchElement,gameObject.transform);
-                        tmp.gameObject.transform.position=new Vector3(i,-1,j);
+                    isCreated = false;
+                    foreach (Transform child in transform) {
+                        if ((child.position.x == i) && (child.position.z == j)) {
+                            isCreated = true;
+                            break;
+                        }
                     }
-                    else{
-                        GameObject tmp=Instantiate(_let,gameObject.transform);
-                        tmp.gameObject.transform.position=new Vector3(i,0,j);
-                    }
+                    if(!isCreated)
+                        if (_loadMap.map[i][j]>=0){
+                        
+                            Toucher tmp=Instantiate(_touchElement,gameObject.transform);
+                            tmp.gameObject.transform.position=new Vector3(i,-1,j);
+                        }
+                        else{
+                            GameObject tmp=Instantiate(_let,gameObject.transform);
+                            tmp.gameObject.transform.position=new Vector3(i,0,j);
+                        }
                 }
             }
         }
