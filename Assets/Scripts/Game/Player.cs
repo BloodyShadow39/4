f4adf62404b2e75d2d7a76c0d24f3916ab305a9d
TechesDePainture <kirillzelen00@gmail.com>
2021-01-10 20:10:44 +0200

    В Player добавлен метод делющий данного игрока отобрааемым на скрине

--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UI;
+using Events;
 
 namespace Game {
     public class Player : MonoBehaviour {
@@ -10,6 +12,14 @@ namespace Game {
         public List<Hero> heroes;
 
         public int gold = 0;
+
+        [SerializeField]
+        private EventDispatcher _playerChanged;
+
+        public void SetPlayerCurrent() {
+            GameScreen.Instance._currentPlayer = this;
+            _playerChanged.Dispatch();
+        }
 
     }
 }
