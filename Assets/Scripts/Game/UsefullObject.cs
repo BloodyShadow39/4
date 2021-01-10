52b353cc70758dc274bf8e62a16aa191436c5b0f
TechesDePainture <kirillzelen00@gmail.com>
2021-01-10 15:36:19 +0200

    Поправлена табуляция добавлено измение карты взависимости от типа обьекта

--- Assets/Scripts/Game/UsefullObject.cs
+++ Assets/Scripts/Game/UsefullObject.cs
@@ -5,148 +5,152 @@ using Game;
 using Values;
 
 namespace Game{
-public class UsefullObject : MonoBehaviour
-{
-    [SerializeField]
-    private ScriptablePickHero _selectedHero;
+    public class UsefullObject : MonoBehaviour
+    {
+        [SerializeField]
+        private ScriptablePickHero _selectedHero;
 
-    private float _distanceForUseful=2f;
+        private float _distanceForUseful=2f;
 
-    [SerializeField]
-    private string _type="";
-    [SerializeField]
-    private int value;
+        [SerializeField]
+        private ScriptableMap _loadMap;
 
-    private void OnMouseDown(){
-        if (_selectedHero.SelectHero != null) {
-        int x=-1;
-        int y=-1;
-        float distance = 
-        ((transform.position.x -1)-_selectedHero.SelectHero.transform.position.x)*
-        ((transform.position.x -1)-_selectedHero.SelectHero.transform.position.x)+
-        ((transform.position.z -1)-_selectedHero.SelectHero.transform.position.z)*
-        ((transform.position.z -1)-_selectedHero.SelectHero.transform.position.z);
-        if(
-            ((transform.position.x -1)-_selectedHero.SelectHero.transform.position.x)*
-        ((transform.position.x -1)-_selectedHero.SelectHero.transform.position.x)+
-        ((transform.position.z)-_selectedHero.SelectHero.transform.position.z)*
-        ((transform.position.z)-_selectedHero.SelectHero.transform.position.z)<distance
-        )
-        {
-            distance=
+        [SerializeField]
+        private string _type="";
+        [SerializeField]
+        private int value;
+
+        private void OnMouseDown(){
+            if (_selectedHero.SelectHero != null) {
+            int x=-1;
+            int y=-1;
+            float distance = 
             ((transform.position.x -1)-_selectedHero.SelectHero.transform.position.x)*
             ((transform.position.x -1)-_selectedHero.SelectHero.transform.position.x)+
+            ((transform.position.z -1)-_selectedHero.SelectHero.transform.position.z)*
+            ((transform.position.z -1)-_selectedHero.SelectHero.transform.position.z);
+            if(
+                ((transform.position.x -1)-_selectedHero.SelectHero.transform.position.x)*
+            ((transform.position.x -1)-_selectedHero.SelectHero.transform.position.x)+
             ((transform.position.z)-_selectedHero.SelectHero.transform.position.z)*
-            ((transform.position.z)-_selectedHero.SelectHero.transform.position.z);
-            x=-1;
-            y=0;
-        }
-        if(
-            ((transform.position.x)-_selectedHero.SelectHero.transform.position.x)*
-        ((transform.position.x)-_selectedHero.SelectHero.transform.position.x)+
-        ((transform.position.z-1)-_selectedHero.SelectHero.transform.position.z)*
-        ((transform.position.z-1)-_selectedHero.SelectHero.transform.position.z)<distance
-        )
-        {
-            distance=
-            ((transform.position.x)-_selectedHero.SelectHero.transform.position.x)*
-            ((transform.position.x)-_selectedHero.SelectHero.transform.position.x)+
-            ((transform.position.z-1)-_selectedHero.SelectHero.transform.position.z)*
-            ((transform.position.z-1)-_selectedHero.SelectHero.transform.position.z);
-            x=0;
-            y=-1;
-        }
-        if(
-            ((transform.position.x)-_selectedHero.SelectHero.transform.position.x)*
-        ((transform.position.x)-_selectedHero.SelectHero.transform.position.x)+
-        ((transform.position.z+1)-_selectedHero.SelectHero.transform.position.z)*
-        ((transform.position.z+1)-_selectedHero.SelectHero.transform.position.z)<distance
-        )
-        {
-            distance=
-            ((transform.position.x)-_selectedHero.SelectHero.transform.position.x)*
-            ((transform.position.x)-_selectedHero.SelectHero.transform.position.x)+
-            ((transform.position.z+1)-_selectedHero.SelectHero.transform.position.z)*
-            ((transform.position.z+1)-_selectedHero.SelectHero.transform.position.z);
-            x=0;
-            y=1;
-        }
-        if(
-        ((transform.position.x+1)-_selectedHero.SelectHero.transform.position.x)*
-        ((transform.position.x+1)-_selectedHero.SelectHero.transform.position.x)+
-        ((transform.position.z)-_selectedHero.SelectHero.transform.position.z)*
-        ((transform.position.z)-_selectedHero.SelectHero.transform.position.z)<distance)
-        {
-            distance=
-            ((transform.position.x+1)-_selectedHero.SelectHero.transform.position.x)*
-            ((transform.position.x+1)-_selectedHero.SelectHero.transform.position.x)+
-            ((transform.position.z)-_selectedHero.SelectHero.transform.position.z)*
-            ((transform.position.z)-_selectedHero.SelectHero.transform.position.z);
-            x=1;
-            y=0;
-        }
-        if(((transform.position.x+1)-_selectedHero.SelectHero.transform.position.x)*
-        ((transform.position.x+1)-_selectedHero.SelectHero.transform.position.x)+
-        ((transform.position.z+1)-_selectedHero.SelectHero.transform.position.z)*
-        ((transform.position.z+1)-_selectedHero.SelectHero.transform.position.z)<distance)
-        {
-            distance=
-            ((transform.position.x+1)-_selectedHero.SelectHero.transform.position.x)*
-            ((transform.position.x+1)-_selectedHero.SelectHero.transform.position.x)+
-            ((transform.position.z+1)-_selectedHero.SelectHero.transform.position.z)*
-            ((transform.position.z+1)-_selectedHero.SelectHero.transform.position.z);
-            x=1;
-            y=1;
-        }
-        if(((transform.position.x-1)-_selectedHero.SelectHero.transform.position.x)*
-        ((transform.position.x-1)-_selectedHero.SelectHero.transform.position.x)+
-        ((transform.position.z+1)-_selectedHero.SelectHero.transform.position.z)*
-        ((transform.position.z+1)-_selectedHero.SelectHero.transform.position.z)<distance)
-        {
-            distance=
-            ((transform.position.x-1)-_selectedHero.SelectHero.transform.position.x)*
-            ((transform.position.x-1)-_selectedHero.SelectHero.transform.position.x)+
-            ((transform.position.z+1)-_selectedHero.SelectHero.transform.position.z)*
-            ((transform.position.z+1)-_selectedHero.SelectHero.transform.position.z);
-            x=-1;
-            y=1;
-        }
-        if(((transform.position.x+1)-_selectedHero.SelectHero.transform.position.x)*
-        ((transform.position.x+1)-_selectedHero.SelectHero.transform.position.x)+
-        ((transform.position.z-1)-_selectedHero.SelectHero.transform.position.z)*
-        ((transform.position.z-1)-_selectedHero.SelectHero.transform.position.z)<distance)
+            ((transform.position.z)-_selectedHero.SelectHero.transform.position.z)<distance
+            )
             {
                 distance=
+                ((transform.position.x -1)-_selectedHero.SelectHero.transform.position.x)*
+                ((transform.position.x -1)-_selectedHero.SelectHero.transform.position.x)+
+                ((transform.position.z)-_selectedHero.SelectHero.transform.position.z)*
+                ((transform.position.z)-_selectedHero.SelectHero.transform.position.z);
+                x=-1;
+                y=0;
+            }
+            if(
+                ((transform.position.x)-_selectedHero.SelectHero.transform.position.x)*
+            ((transform.position.x)-_selectedHero.SelectHero.transform.position.x)+
+            ((transform.position.z-1)-_selectedHero.SelectHero.transform.position.z)*
+            ((transform.position.z-1)-_selectedHero.SelectHero.transform.position.z)<distance
+            )
+            {
+                distance=
+                ((transform.position.x)-_selectedHero.SelectHero.transform.position.x)*
+                ((transform.position.x)-_selectedHero.SelectHero.transform.position.x)+
+                ((transform.position.z-1)-_selectedHero.SelectHero.transform.position.z)*
+                ((transform.position.z-1)-_selectedHero.SelectHero.transform.position.z);
+                x=0;
+                y=-1;
+            }
+            if(
+                ((transform.position.x)-_selectedHero.SelectHero.transform.position.x)*
+            ((transform.position.x)-_selectedHero.SelectHero.transform.position.x)+
+            ((transform.position.z+1)-_selectedHero.SelectHero.transform.position.z)*
+            ((transform.position.z+1)-_selectedHero.SelectHero.transform.position.z)<distance
+            )
+            {
+                distance=
+                ((transform.position.x)-_selectedHero.SelectHero.transform.position.x)*
+                ((transform.position.x)-_selectedHero.SelectHero.transform.position.x)+
+                ((transform.position.z+1)-_selectedHero.SelectHero.transform.position.z)*
+                ((transform.position.z+1)-_selectedHero.SelectHero.transform.position.z);
+                x=0;
+                y=1;
+            }
+            if(
             ((transform.position.x+1)-_selectedHero.SelectHero.transform.position.x)*
             ((transform.position.x+1)-_selectedHero.SelectHero.transform.position.x)+
+            ((transform.position.z)-_selectedHero.SelectHero.transform.position.z)*
+            ((transform.position.z)-_selectedHero.SelectHero.transform.position.z)<distance)
+            {
+                distance=
+                ((transform.position.x+1)-_selectedHero.SelectHero.transform.position.x)*
+                ((transform.position.x+1)-_selectedHero.SelectHero.transform.position.x)+
+                ((transform.position.z)-_selectedHero.SelectHero.transform.position.z)*
+                ((transform.position.z)-_selectedHero.SelectHero.transform.position.z);
+                x=1;
+                y=0;
+            }
+            if(((transform.position.x+1)-_selectedHero.SelectHero.transform.position.x)*
+            ((transform.position.x+1)-_selectedHero.SelectHero.transform.position.x)+
+            ((transform.position.z+1)-_selectedHero.SelectHero.transform.position.z)*
+            ((transform.position.z+1)-_selectedHero.SelectHero.transform.position.z)<distance)
+            {
+                distance=
+                ((transform.position.x+1)-_selectedHero.SelectHero.transform.position.x)*
+                ((transform.position.x+1)-_selectedHero.SelectHero.transform.position.x)+
+                ((transform.position.z+1)-_selectedHero.SelectHero.transform.position.z)*
+                ((transform.position.z+1)-_selectedHero.SelectHero.transform.position.z);
+                x=1;
+                y=1;
+            }
+            if(((transform.position.x-1)-_selectedHero.SelectHero.transform.position.x)*
+            ((transform.position.x-1)-_selectedHero.SelectHero.transform.position.x)+
+            ((transform.position.z+1)-_selectedHero.SelectHero.transform.position.z)*
+            ((transform.position.z+1)-_selectedHero.SelectHero.transform.position.z)<distance)
+            {
+                distance=
+                ((transform.position.x-1)-_selectedHero.SelectHero.transform.position.x)*
+                ((transform.position.x-1)-_selectedHero.SelectHero.transform.position.x)+
+                ((transform.position.z+1)-_selectedHero.SelectHero.transform.position.z)*
+                ((transform.position.z+1)-_selectedHero.SelectHero.transform.position.z);
+                x=-1;
+                y=1;
+            }
+            if(((transform.position.x+1)-_selectedHero.SelectHero.transform.position.x)*
+            ((transform.position.x+1)-_selectedHero.SelectHero.transform.position.x)+
             ((transform.position.z-1)-_selectedHero.SelectHero.transform.position.z)*
-            ((transform.position.z-1)-_selectedHero.SelectHero.transform.position.z);
-            x=1;
-            y=-1;
-            }
-            distance=((transform.position.x)-_selectedHero.SelectHero.transform.position.x)*
-        ((transform.position.x)-_selectedHero.SelectHero.transform.position.x)+
-        ((transform.position.z)-_selectedHero.SelectHero.transform.position.z)*
-        ((transform.position.z)-_selectedHero.SelectHero.transform.position.z);
-                if(distance<=_distanceForUseful){
-                    Use();
+            ((transform.position.z-1)-_selectedHero.SelectHero.transform.position.z)<distance)
+                {
+                    distance=
+                ((transform.position.x+1)-_selectedHero.SelectHero.transform.position.x)*
+                ((transform.position.x+1)-_selectedHero.SelectHero.transform.position.x)+
+                ((transform.position.z-1)-_selectedHero.SelectHero.transform.position.z)*
+                ((transform.position.z-1)-_selectedHero.SelectHero.transform.position.z);
+                x=1;
+                y=-1;
                 }
-                else
-                    if (_selectedHero.SelectHero.GetTarget() != new Vector2Int((int)transform.position.x+x,(int)transform.position.z+y))
-                        _selectedHero.SelectHero.SetTouch((int)transform.position.x+x,(int)transform.position.z+y);
+                distance=((transform.position.x)-_selectedHero.SelectHero.transform.position.x)*
+            ((transform.position.x)-_selectedHero.SelectHero.transform.position.x)+
+            ((transform.position.z)-_selectedHero.SelectHero.transform.position.z)*
+            ((transform.position.z)-_selectedHero.SelectHero.transform.position.z);
+                    if(distance<=_distanceForUseful){
+                        Use();
+                    }
                     else
-                        _selectedHero.SelectHero.MoveAllWay();
-            }
-            else {
-                Debug.LogError("Hero not pick");
-            }
-    }
+                        if (_selectedHero.SelectHero.GetTarget() != new Vector2Int((int)transform.position.x+x,(int)transform.position.z+y))
+                            _selectedHero.SelectHero.SetTouch((int)transform.position.x+x,(int)transform.position.z+y);
+                        else
+                            _selectedHero.SelectHero.MoveAllWay();
+                }
+                else {
+                    Debug.LogError("Hero not pick");
+                }
+        }
 
-    private void Use(){
-        if(_type=="Gold"||_type=="gold"){
-            _selectedHero.SelectHero.player.gold+=value;
-            Destroy(gameObject);
+        private void Use(){
+            if(_type=="Gold"||_type=="gold"){
+                _selectedHero.SelectHero.player.gold+=value;
+                _loadMap.mapOfObjects[(int)transform.position.x][(int)transform.position.z] = _loadMap.GetEmpty();
+                Destroy(gameObject);
+            }
         }
     }
-}
 }
