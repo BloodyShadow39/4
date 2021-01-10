7323f97ee9f561ef19468c96915b3a22e29d1651
TechesDePainture <kirillzelen00@gmail.com>
2021-01-10 19:35:09 +0200

    Создан класс игрок (замена ассетовому сохранинению)

---
+++ Assets/Scripts/Game/Player.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game {
+    public class Player : MonoBehaviour {
+
+        public string playerName = "";
+
+        public List<Hero> heroes;
+
+        public int gold = 0;
+
+    }
+}
