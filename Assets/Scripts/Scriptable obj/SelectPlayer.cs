a91f420af03afd3230781818a56c5c8d95d2b163
TechesDePainture <kirillzelen00@gmail.com>
2021-01-10 19:36:45 +0200

    Создан обьект описание выбраного игрока

---
+++ Assets/Scripts/Scriptable obj/SelectPlayer.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game;
+
+[CreateAssetMenu(fileName = "Select Player")]
+public class SelectPlayer : ScriptableObject
+{
+    public Player currentPlayer;
+}
