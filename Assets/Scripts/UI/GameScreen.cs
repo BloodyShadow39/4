bbc1130b3ce5cf3c47e0c02ba5c93ab7c52799f9
TechesDePainture <kirillzelen00@gmail.com>
2021-01-10 19:39:47 +0200

    Изменения GameSceen касаемые выбираемого игрока

--- Assets/Scripts/UI/GameScreen.cs
+++ Assets/Scripts/UI/GameScreen.cs
@@ -8,19 +8,43 @@ using Events;
 namespace UI {
     public class GameScreen : MonoBehaviour {
 
+        public static GameScreen Instance;
+
         [SerializeField]
         private List<RawImage> _heroIcons;
 
         [SerializeField]
-        private ScriptablePlayer _currentPlayer;
+        private SelectPlayer _currentPlayer;
 
         [SerializeField]
         private EventListener _playerChanged;
 
+        private void Awake() {
+            if (Instance != null) {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private void OnEnable() {
+            _playerChanged.OnEventHappened += FillIconsHeroes;
+        }
+
+        private void OnDisable() {
+            _playerChanged.OnEventHappened -= FillIconsHeroes;
+        }
+
         private void FillIconsHeroes() {
-            for(int i = 0; i < _currentPlayer.heroes.Count; i++) {
-                if (_heroIcons.Count > i)
-                    _heroIcons[i].texture = _currentPlayer.heroes[i].RenderCamera().Render();
+            
+            for(int i = 0; i < _currentPlayer.currentPlayer.heroes.Count; i++) {
+                Debug.Log($"{ _heroIcons};{i}");
+                if (_heroIcons.Count > i) {
+                    _heroIcons[i].texture = _currentPlayer.currentPlayer.heroes[i].RenderCamera().Render();
+                    Debug.Log(true);
+                }
                 else
                     break;
             }
