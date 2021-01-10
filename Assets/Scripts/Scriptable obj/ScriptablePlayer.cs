32148b27b2fecde69b508f8b5ecbec6496cbc22f
TechesDePainture <kirillzelen00@gmail.com>
2021-01-10 18:30:08 +0200

    Добавлен список героев принадлежайщий игроку

--- Assets/Scripts/Scriptable obj/ScriptablePlayer.cs
+++ Assets/Scripts/Scriptable obj/ScriptablePlayer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Game;
 
 
 namespace Values{
@@ -8,6 +9,8 @@ namespace Values{
     public class ScriptablePlayer : ScriptableObject
     {
         public string playerName="";
+
+        public List<Hero> heroes;
 
         public int gold=0;
     }
