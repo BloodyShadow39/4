6966be56a758fe2b102c8e1cf14c3044ae92658c
TechesDePainture <kirillzelen00@gmail.com>
2021-01-11 22:33:41 +0200

    Исправление табуляции

--- Assets/Scripts/Game/Entity.cs
+++ Assets/Scripts/Game/Entity.cs
@@ -2,49 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Entity : MonoBehaviour
-{
-    public string discription;
+namespace Game {
+    public class Entity : MonoBehaviour {
+        public string discription;
 
-    [Range(0f, 100f)]
-    public float distanceAttack = 1f;
+        [Range(0f, 100f)]
+        public float distanceAttack = 1f;
 
-    [Range(0f, 1000f)]
-    public float heath = 100f;
+        [Range(0f, 1000f)]
+        public float heath = 100f;
 
-    [Range(0f, 100f)]
-    public float mindamage = 1f;
+        [Range(0f, 100f)]
+        public float mindamage = 1f;
 
-    [Range(0f,100f)]
-    public float maxdamage = 2f;
+        [Range(0f, 100f)]
+        public float maxdamage = 2f;
 
-    [Range(0,100000)]
-    public int costOfGold = 0;
+        [Range(0, 100000)]
+        public int costOfGold = 0;
 
-    [Range(0,100)]
-    public int costOfOther = 0;
+        [Range(0, 100)]
+        public int costOfOther = 0;
 
-    [Range(0,100)]
-    public int units = 1;
+        [Range(0, 100)]
+        public int units = 1;
 
-    [Range(0,100)]
-    public int movePoints;
-    
-    [Range(0, 100)]
-    public int localMovePoints;
-    
-    [Range(0, 300)]
-    public int manaPoints;
+        [Range(0, 100)]
+        public int movePoints;
 
-    [Range(0,200)]
-    public int countOfArrows;
+        [Range(0, 100)]
+        public int localMovePoints;
 
-    [Range(0, 10000)]
-    public int expirianceOfOnceUnit;
+        [Range(0, 300)]
+        public int manaPoints;
 
-    public string ability;
+        [Range(0, 200)]
+        public int countOfArrows;
 
-    public void Attack() {
+        [Range(0, 10000)]
+        public int expirianceOfOnceUnit;
 
+        public string ability;
+
+        public void Attack() {
+
+        }
     }
 }
